19ff2cf7304ef5cbcb079c23ca96fc296051689d
JULIANA <julianabetancur320@gmail.com>
2024-08-22 13:54:00 -0500

    crear metodos init, mark_completes, add_tag, __str__

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,8 +1,19 @@
 # TODO: Add code here
 class Todo:
+
     def __init__(self, code_id: int, title: str, description: str, completed: bool, tags: list[str]) -> None:
         self.code_id: int = code_id
         self.title: str = title
         self.description: str = description
         self.completed: bool = False
         self.tags: list = []
+
+    def mark_completes(self):
+        self.completed: bool = True
+    
+    def add_tag (self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+    
+    def __str__(self) -> str:
+        return f"{self.code_id - self.title}"
